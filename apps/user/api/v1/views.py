eaba8c7153e3326070b8145e2eea48a783c8ca94
sudip-khanal <sudipkhanal0777@gmail.com>
2024-07-01 15:33:50 +0545

    added action decorator

--- apps/user/api/v1/views.py
+++ apps/user/api/v1/views.py
@@ -14,39 +14,20 @@ from .serializers import (
 class UserViewSet(viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class=RegisterSerializer
-    """
-        Determine which serializer class to use based on the action being performed.
-    """
-    def get_serializer_class(self):
-        if self.action == 'register':
-            return RegisterSerializer
-        elif self.action == 'verify_email':
-            return VerifyEmailSerializer
-        elif self.action == 'forgot_password':
-            return ForgotPasswordSerializer
-        elif self.action == 'reset_password':
-            return ResetPasswordSerializer
-        elif self.action == 'login':
-            return LoginSerializer
-        elif self.action == 'change_password':
-            return ChangePasswordSerializer
-        elif self.action== 'logout':
-            return LogoutSerializer
-        return super().get_serializer_class()
     
     # Action to register a new user.
-    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny],serializer_class=RegisterSerializer)
     def register(self, request):
-        serializer = self.get_serializer(data=request.data, context={'request': request})
+        serializer = RegisterSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response({'message': 'Registered successfully. Please verify your email.'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     # Action to verify the email using the provided token.
-    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny],serializer_class=VerifyEmailSerializer)
     def verify_email(self, request):
-        serializer = self.get_serializer(data=request.data)
+        serializer = VerifyEmailSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         token = serializer.validated_data['token']
         try:
@@ -58,19 +39,18 @@ class UserViewSet(viewsets.GenericViewSet):
             return Response({'detail': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
 
     #  Action to initiate the password reset process.
-    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny],serializer_class=ForgotPasswordSerializer)
     def forgot_password(self, request):
-        serializer = self.get_serializer(data=request.data)
+        serializer = ForgotPasswordSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(request)
         return Response({'detail': 'Password reset link sent'}, status=status.HTTP_200_OK)
     
     # Action to reset the password using the provided token and UID.
-    # @action(detail=False, methods=['post'], permission_classes=[AllowAny])
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny],serializer_class=ResetPasswordSerializer)
     def reset_password(self, request, uid=None, token=None):
-        serializer = self.get_serializer(data=request.data)
+        serializer = ResetPasswordSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
         try:
             uid = urlsafe_base64_decode(uid).decode()
             user = User.objects.get(pk=uid)
@@ -84,25 +64,25 @@ class UserViewSet(viewsets.GenericViewSet):
             return Response({'detail': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
         
     # Action to authenticate and login a user.
-    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny],serializer_class=LoginSerializer)
     def login(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
     
     # Action to logout the authenticated user by deleting their token.
-    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated],serializer_class=LogoutSerializer)
     def logout(self,request):
-        serializer = self.get_serializer(data=request.data)
+        serializer = LogoutSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({'detail': 'Logout successful'}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     # Action to change the authenticated user's password.
-    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated],serializer_class=ChangePasswordSerializer)
     def change_password(self, request):
-        serializer = self.get_serializer(data=request.data)
+        serializer = ChangePasswordSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"message": "Password changed successfully"}, status=status.HTTP_200_OK)
