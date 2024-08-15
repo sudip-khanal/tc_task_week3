643c3997522e24f1305e3d7b79593e331c82b31d
sudip-khanal <101724348+sudip-khanal@users.noreply.github.com>
2024-08-15 15:56:08 +0545

    Delete apps directory

--- apps/review/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Review
-
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('id','book', 'user', 'review_text', 'rating','created_at')
-
-admin.site.register(Review,ReviewAdmin)
--- apps/review/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from apps.review.views import ReviewViewSet
-
-router=DefaultRouter()
-router.register('', ReviewViewSet, basename='review')
-urlpatterns = router.urls
-
